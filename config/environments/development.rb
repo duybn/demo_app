71460b6d4e545b76f6b03544c73a1a396d060670
smham <duy.buinhat@employmenthero.com>
2024-03-17 18:28:17 +0700

    Add config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://13.251.114.239:3000/cable"
 end
